3bd8acf497f03df1e645a3e8716b678197a0ae75
jrfranco08 <rodrigofrancojr@hotmail.com>
2025-11-04 12:05:33 -0300

    finalização do sistema controle de horas

--- src/controllers/controle-de-horas.controller.js
+++ src/controllers/controle-de-horas.controller.js
@@ -2,15 +2,13 @@ const ControleDeHoras = require("../models/controle-de-horas.model");
 
 exports.buscarBancoDeHorasGeral = async (req, res) => {
   try {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
     const mesAno = req.query.mesAno;
 
     if (!mesAno) {
       return res.status(400).json({ error: "O parâmetro 'mesAno' é obrigatório." });
     }
 
-    const result = await ControleDeHoras.buscarBancoDeHorasGeral(page, limit, mesAno);
+    const result = await ControleDeHoras.buscarBancoDeHorasGeral(mesAno);
 
     res.status(200).json({
       success: true,
--- src/models/controle-de-horas.model.js
+++ src/models/controle-de-horas.model.js
@@ -1,39 +1,24 @@
 const pool = require("../config/database");
 
 const ControleDeHoras = {
-    async buscarBancoDeHorasGeral(page = 1, limit = 10, mesAno) {
+    async buscarBancoDeHorasGeral(mesAno) {
 
         if (!mesAno) {
             throw new Error("O parâmetro 'mesAno' é obrigatório.");
         }
 
-        const offset = (page - 1) * limit;
-
         const query = `
-      SELECT id, nome_funcionario, saldo_banco_final
-      FROM relatorio_banco_horas
-      WHERE mes_referencia = $3
-      ORDER BY id
-      LIMIT $1 OFFSET $2
+        SELECT r.nome_funcionario, saldo_banco_final
+        FROM relatorio_banco_horas r
+		inner join funcionarios f on r.idfunc = f.id
+		WHERE r.mes_referencia = $1
+	    AND f.cargo = 'Motorista'
+	    ORDER BY r.id
     `;
-        const result = await pool.query(query, [limit, offset, mesAno]);
-
-        const countQuery = `
-      SELECT COUNT(*) AS total
-      FROM relatorio_banco_horas 
-      WHERE mes_referencia = $1
-    `;
-        const countResult = await pool.query(countQuery, [mesAno]);
-
-        const total = parseInt(countResult.rows[0].total);
-        const totalPages = Math.ceil(total / limit);
+        const result = await pool.query(query, [mesAno]);
 
         return {
             data: result.rows,
-            total,
-            totalPages,
-            page,
-            limit
         };
     },
 };

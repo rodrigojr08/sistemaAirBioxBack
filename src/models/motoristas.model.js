c90e4ae9a04494d298a62c94f761c30e8e54223f
jrfranco08 <rodrigofrancojr@hotmail.com>
2026-02-13 11:47:06 -0300

    merge

--- src/models/motoristas.model.js
+++ src/models/motoristas.model.js
@@ -2,12 +2,8 @@ const pool = require("../config/database");
 
 const MotoristasModel = {
     buscarMotoristas: async () => {
-<<<<<<< HEAD
-        const result = await pool.query(`select * from funcionarios where cargo = 'Motorista' and ativo = true order by nome`);
-=======
         const result = await pool.query(`select id, TRIM(nome) AS nome, TRIM(email) AS email, TRIM(cargo) AS cargo, 
             TRIM(usuario) AS usuario from funcionarios where cargo = 'Motorista' and ativo = true order by nome`);
->>>>>>> feature/master/tabuleiro
         return result.rows;
     }
 }
